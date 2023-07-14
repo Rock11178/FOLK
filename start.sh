391e8b9dfe0eb4eca5a2cd007877c41f5527010a
Rock11178 <128242010+Rock11178@users.noreply.github.com>
2023-07-14 13:30:16 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/abhinand2608/thefilmyspotbot.git /thefilmyspotbot
+  git clone https://github.com/Rock11178/FOLK.git /FOLK
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /thefilmyspotbot
+  git clone $UPSTREAM_REPO /FOLK
 fi
 cd /thefilmyspotbot
 pip3 install -U -r requirements.txt
-echo "Starting ThefilmyspotBot ✅...."
+echo "Starting FOLK ✅...."
 python3 bot.py
