adb7bd103019ef90284d46d0550eae357cd149d1
Rock11178 <128242010+Rock11178@users.noreply.github.com>
2023-07-14 13:42:07 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /FOLK
 fi
-cd /thefilmyspotbot
+cd /FOLK
 pip3 install -U -r requirements.txt
 echo "Starting FOLK ✅...."
 python3 bot.py
